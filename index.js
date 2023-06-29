6adb5fc1bdbf79120a660589ff4367d78c23590f
haydercharfeddin <135696166+haydercharfeddin@users.noreply.github.com>
2023-06-30 00:07:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var imagepr=document.querySelector('.img')
+var colorbg=document.querySelector('.container')
+
+function phones(phone){
+    imagepr.src=phone
+}
+function colors(color){
+    colorbg.style.background=color
+}
+
+
